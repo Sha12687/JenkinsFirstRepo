7664ac1242a6f63dd07453b432bebebff6ac5f0a
Shashant <shashantchaudhary999@gmail.com>
2023-12-04 12:49:00 +0530

    second commit

---
+++ JenkinDemoProject/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JenkinDemoProject.Models;
+namespace JenkinDemoProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductModel model = new ProductModel();
+            model.ProductId = 100;
+            model.ProductName = "ABC";
+            return View();
+        }
+    }
+}
---
+++ JenkinDemoProject/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JenkinDemoProject.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+    }
+}
